e77e14019224048476569454c903e4aa164f4c73
Nonthasak Laoluerat <nonthasak.l@gmail.com>
2025-02-21 23:03:13 +0700

    upd: add meta tag to set view port

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, height=device-height, initial-scale=1, minimum-scale=1, maximum-scale=1"
+      />
       <LineProvider>
         <body
           className={`${prompt.className} antialiased bg-gradient-to-br from-base-300 via-base-100 to-base-300`}
